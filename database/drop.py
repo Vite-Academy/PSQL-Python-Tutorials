a43ac758ed0bb89a5ead642a1353704e04c565c3
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-08-26 17:37:17 +0500

    done

---
+++ database/drop.py
@@ -0,0 +1,18 @@
+# Note: the module name is psycopg, not psycopg3
+import psycopg
+
+# Connect to an existing database
+with psycopg.connect("dbname=postgres user=postgres host=localhost port=5432 password=080199") as conn:
+
+    conn.autocommit = True
+
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+
+        # query to create a database
+        sql = "DROP DATABASE your_db"
+        
+
+        # executing above query
+        cur.execute(sql)
+        print("Drop Database!!")
---
+++ database/rename.py
@@ -0,0 +1,18 @@
+# Note: the module name is psycopg, not psycopg3
+import psycopg
+
+# Connect to an existing database
+with psycopg.connect("dbname=postgres user=postgres host=localhost port=5432 password=080199") as conn:
+
+    conn.autocommit = True
+
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+
+        # query to create a database
+        sql = "ALTER DATABASE my_db RENAME TO your_db"
+        
+
+        # executing above query
+        cur.execute(sql)
+        print("Rename Database!!")
